a4b8b7505cc7cf81423affd64fb12307d15b568a
gabipayan <gapayan26@gmail.com>
2025-04-09 11:36:34 -0600

    mockup

--- Portfolio/src/app.jsx
+++ Portfolio/src/app.jsx
@@ -7,6 +7,7 @@
    import Portfolio from './pages/Portfolio';
    import Contact from './pages/Contact';
    import Resume from './pages/Resume';
+   import './style.css';
 
    const App = () => {
      return (
